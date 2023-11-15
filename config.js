35ae17dc70e8ecb70cf96f2d5534ce534215ccfb
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-11-15 21:04:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.autoreadgc = true;
 
 //
 global.prefa = ["."];                                    //Default prefix here. you can change if you want.
-global.port = process.env.PORT || "10000";
+global.port = process.env.PORT || "3000";
 global.mongodb = process.env.MONGODB || "mongodb+srv://tanujairam:tanujairam@whatsapp.yixkzgb.mongodb.net/?retryWrites=true&w=majority";
 //
 global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
